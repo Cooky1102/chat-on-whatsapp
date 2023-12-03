25a1230a1dd1234da8f101443d5fc17c3e1a1eed
cooky <437487609@qq.com>
2023-12-03 15:36:59 +0800

    第三种形式测试唤起

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <h3>https://wa.me</h3>
       <div>
         <a
           aria-label="Chat on WhatsApp"
-          href="https://wa.me/85294507775?text=I'm%20interested%20in%20your%20e-cigarette
+          href="https://wa.me/85252269794?text=I'm%20interested%20in%20your%20e-cigarette
 %20for%20sale"
         >
           <img
@@ -16,8 +17,21 @@ function App() {
         </a>
       </div>
 
-      <br />
+      <h3>https://api.whatspp.com</h3>
+      <div>
+        <a
+          aria-label="Chat on WhatsApp"
+          href="https://api.whatsapp.com/send?phone=85252269794&text=I'm%20interested%20in%20your%20e-cigarette
+%20for%20sale"
+        >
+          <img
+            alt="Chat on WhatsApp"
+            src="/chat-on-whatsapp/WhatsAppButtonGreenLarge.svg"
+          />
+        </a>
+      </div>
 
+      <h3>whatsapp://send?phone=85252269794</h3>
       <div>
         <a
           aria-label="Chat on WhatsApp"
