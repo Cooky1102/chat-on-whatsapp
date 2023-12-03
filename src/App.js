753b892836eef8372bf9be5fb425e94b6e56339e
cooky <437487609@qq.com>
2023-12-03 14:54:55 +0800

    第二种形式测试唤起

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <a aria-label="Chat on WhatsApp" href="https://wa.me/85294507775?text=I'm%20interested%20in%20your%20e-cigarette
-%20for%20sale">
-        <img
+      <div>
+        <a
+          aria-label="Chat on WhatsApp"
+          href="https://wa.me/85294507775?text=I'm%20interested%20in%20your%20e-cigarette
+%20for%20sale"
+        >
+          <img
             alt="Chat on WhatsApp"
             src="/chat-on-whatsapp/WhatsAppButtonGreenLarge.svg"
-        />
-      </a>
+          />
+        </a>
+      </div>
+
+      <br />
+
+      <div>
+        <a
+          aria-label="Chat on WhatsApp"
+          href="https://api.whatsapp.com/send?phone=85294507775&text=I'm%20interested%20in%20your%20e-cigarette
+%20for%20sale"
+        >
+          <img
+            alt="Chat on WhatsApp"
+            src="/chat-on-whatsapp/WhatsAppButtonGreenLarge.svg"
+          />
+        </a>
+      </div>
     </div>
   );
 }
